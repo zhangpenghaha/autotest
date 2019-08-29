8b253e4c147aaa071f9ad048dd3df0059d457e8c
zhangpenghaha <zhangpeng1835@163.com>
2019-08-29 23:29:00 +0800

    自动化平台2

--- autotest/settings.py
+++ autotest/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
         'NAME' : 'autotest',
         'USER' : 'root',
         'PASSWORD' : '123456',
-        'HOST' : '127.0.0.1',
-        'PORT' : '3306'
+        'HOST' : 'localhost',
+        'PORT' : '3306',
 
     }
 }
