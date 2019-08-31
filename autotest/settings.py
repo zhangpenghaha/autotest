f25bc25eda4e4bb76f2770c493a0205f255b5273
zhangpeng <zhangpeng1835@163.com>
2019-08-31 23:39:41 +0800

    自动化

--- autotest/settings.py
+++ autotest/settings.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-   ## 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -128,5 +128,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT='/var/www/test6/static/'
+STATIC_ROOT='/root/autotest/static/'
 STATIC_URL = '/static/'
